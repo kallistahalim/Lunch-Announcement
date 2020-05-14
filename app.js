9b6a82133492b130138f22b940e94f2eacc075d5
kallistahalim <kallistahalim@gmail.com>
2020-05-14 00:51:56 -0700

    set up complete

---
+++ app.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA0SRXYJT8nBSRzQSJkbbUBdRdWF2YHTr4",
+    authDomain: "lunch-announcement.firebaseapp.com",
+    databaseURL: "https://lunch-announcement.firebaseio.com",
+    projectId: "lunch-announcement",
+    storageBucket: "lunch-announcement.appspot.com",
+    messagingSenderId: "163601287102",
+    appId: "1:163601287102:web:da4650a955a25b2d2d9bef",
+    measurementId: "G-8373ZVP2VQ"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
